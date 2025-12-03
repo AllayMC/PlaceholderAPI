b32d72c13967b2b88088850f195781eb43fed4ee
daoge_cmd <3523206925@qq.com>
2025-12-03 19:42:54 +0800

    feat: bump allay gradle to 0.2.0 and version to 0.1.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
     id("java-library")
     id("com.vanniktech.maven.publish") version "0.34.0"
     id("com.gradleup.shadow") version "9.2.2"
-    id("org.allaymc.gradle.plugin") version "0.1.2"
+    id("org.allaymc.gradle.plugin") version "0.2.0"
 }
 
 group = "org.allaymc"
-version = "0.1.1-SNAPSHOT"
+version = "0.1.2-SNAPSHOT"
 description = "The official placeholder api for Allay"
 
 allay {
